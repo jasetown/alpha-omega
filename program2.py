49cf85cd6f475a10922b8d5e58bd1473f27b3e00
jasetown <142293489+jasetown@users.noreply.github.com>
2024-09-10 15:28:13 -0500

    Delete program2.py

--- program2.py
+++
@@ -1,9 +0,0 @@
-# Program to convert Celsius to Fahrenheit
-
-celsius = float(input("Enter temperature in Celsius: "))
-
-# Convery C to F
-
-fahrenheit = (celsius * 9/5) + 32
-
-print(f"{celsius}°C is equal to {fahrenheit:.2f}°F.")
